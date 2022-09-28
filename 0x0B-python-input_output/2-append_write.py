03df5e47fc75ea9143d8f5efd4e19644614f8f58
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-09-29 01:38:09 +0300

    t2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,18 +1,10 @@
-
-dule to append to a file"""
-
-
-
+#!/usr/bin/python3
+"""Module to append to a file"""
 
 
 def append_write(filename="", text=""):
-
-        '''Appends a string to text file and returns length of appended string'''
-
-            with open(filename, 'a', encoding="utf-8") as f:
-
-                    for char in text:
-
-                                f.write(char)
-
-                                    return len(text)
+    '''Appends a string to text file and returns length of appended string'''
+    with open(filename, 'a', encoding="utf-8") as f:
+        for char in text:
+            f.write(char)
+    return len(text)
