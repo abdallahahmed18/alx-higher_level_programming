eba90a03e5c66f3f8f3b82ac4e43d56c31204d4a
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-09-29 01:48:19 +0300

    t9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
-"""Module to return object from JSON data"""
-
-
+""" Module that defines the class Student
+"""
 
 
+class Student:
+    """ Class to create student instances """
 
-def from_json_string(my_str):
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-        '''Returns python object form JSON'''
-
-            import json
-
-                return json.loads(my_str)
+    def to_json(self):
+        """ Method that returns directory description """
+        return self.__dict__.copy()
