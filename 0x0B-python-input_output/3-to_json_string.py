4de1e2a5833e0e16fc4ddc03b56990b9005e99c0
abdallahahmed18 <abdallaahmedabdash@gmail.com>
2022-09-28 15:33:18 -0700

    mega add

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Module to return JSON representation of an object"""
+
+
+
+
+
+def to_json_string(my_obj):
+
+        '''Returns JSON'''
+
+            import json
+
+                return json.dumps(my_obj)
