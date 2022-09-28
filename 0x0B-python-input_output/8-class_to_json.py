773f7bc10742eaa58b9430eb4871236b736daa24
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-09-29 01:46:49 +0300

    t8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-
-"""Module to return object from JSON data"""
-
+"""Defines a Python class-to-JSON function."""
 
 
-
-
-def from_json_string(my_str):
-
-        '''Returns python object form JSON'''
-
-            import json
-
-                return json.loads(my_str)
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__ 
