467ba48fce00ec483a8b859520f4f0bda083996a
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-09-29 01:44:45 +0300

    t6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
-"""Module to return object from JSON data"""
-
+"""Module to extract python object from a JSON file"""
 
 
-
-
-def from_json_string(my_str):
-
-        '''Returns python object form JSON'''
-
-            import json
-
-                return json.loads(my_str)
-
+def load_from_json_file(filename):
+    '''Reads a json file and extracts python object'''
+    import json
+    with open(filename, 'r', encoding="utf-8") as f:
+        content = json.loads(f.read())
+    return content
