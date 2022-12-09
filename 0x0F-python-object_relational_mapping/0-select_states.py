e22422db75c9c93e57b4d381e831b65b41c27eae
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-12-09 10:42:55 +0300

    t0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,36 +5,36 @@ from sys import argv
 
 
 def get_dbase():
-        """Takes arguments argv and list from database
-            Arguments:
-                    argv[1]: mysql username
-                            argv[2]: mysql password
-                                    argv[3]: database name
-                                        """
-                                            dbase = MySQLdb.connect(host="localhost",
-                                                                                port=3306,
-                                                                                                            user=argv[1],
-                                                                                                                                        passwd=argv[2],
-                                                                                                                                                                    db=argv[3],
-                                                                                                                                                                                                charset="utf8"
-                                                                                                                                                                                                                            )
+    """Takes arguments argv and list from database
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            charset="utf8"
+                            )
 
-                                                # Getting a cursor
-                                                    dbase_cur = dbase.cursor()
+    # Getting a cursor
+    dbase_cur = dbase.cursor()
 
-                                                        # Esecuting dbase queries
-                                                            dbase_cur.execute("SELECT * FROM states ORDER BY id")
+    # Esecuting dbase queries
+    dbase_cur.execute("SELECT * FROM states ORDER BY id")
 
-                                                                # Fetches all the rows of a query result
-                                                                    query_rows = dbase_cur.fetchall()
+    # Fetches all the rows of a query result
+    query_rows = dbase_cur.fetchall()
 
-                                                                        # Print result one by one
-                                                                            for rows in query_rows:
-                                                                                        print(rows)
+    # Print result one by one
+    for rows in query_rows:
+        print(rows)
 
-                                                                                            dbase_cur.close()
-                                                                                                dbase.close()
+    dbase_cur.close()
+    dbase.close()
 
 
-                                                                                                if __name__ == '__main__':
-                                                                                                        get_dbase()
+if __name__ == '__main__':
+    get_dbase()
