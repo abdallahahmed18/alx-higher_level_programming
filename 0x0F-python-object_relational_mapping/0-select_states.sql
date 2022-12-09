c8bbd3c74cc9288796cabe8357e1fe61ad64758d
Abdallah Ahmed <35594355+abdallahahmed18@users.noreply.github.com>
2022-12-10 00:35:17 +0300

    t00

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -7,10 +7,3 @@ CREATE TABLE IF NOT EXISTS states (
     PRIMARY KEY (id)
 );
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
-
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
